78a0f733756d368fb32d32ea6e7c674fd0af2239
mariamiroslavova <60809804+mariamiroslavova@users.noreply.github.com>
2020-02-08 13:14:43 +0200

    Add files via upload

---
+++ zvezdichki.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            { 
+             double n = double.Parse(Console.ReadLine());
+                Console.Write("\\");
+                for (int i = 1; i <= n; i++)
+
+                {
+                    Console.Write("*");
+
+                }
+                Console.Write("/");
+            }
+        }
+    }
+}
